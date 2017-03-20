d622c12ecadf4ff71a86f903b54b5ed953f2ff77
Dumke <Markus.Dumke@campus.lmu.de>
2017-03-20 17:52:24 +0100

    add addPanel.R

--- R/leafletSidebar.R
+++ R/leafletSidebar.R
@@ -15,7 +15,6 @@ leafletSidebarDependency <- function() {
 #' Types of events: Opening, closing and content
 #'
 #' @param map a map widget object
-#' @param pane html content as character
 #' @param options list of options, e.g. `options = list(position = "right")`
 #' @return modified map
 #' @import htmltools
@@ -26,16 +25,31 @@ leafletSidebarDependency <- function() {
 #' library(leaflet)
 #' pane1 <- ' <h1 class="sidebar-header"> Tab1
 #' <span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
-#' <p>A responsive sidebar for mapping libraries like <a href="http://leafletjs.com/">Leaflet</a> or <a href="http://openlayers.org/">OpenLayers</a>.</p>
+#' <p>A responsive sidebar for <a href="http://leafletjs.com/">Leaflet</a>.</p>
 #' '
 #' pane2 <- '<h1 class="sidebar-header">Tab2<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>'
-#' leaflet() %>% addTiles() %>% addLeafletSidebar(options = list(position = "right"), pane1, pane2)
+#' leaflet() %>% addTiles() %>% addLeafletSidebar(options = list(position = "right")) %>% 
+#'   addPanel(layerId = "Tab1", tab_name = "Tab1", pane = pane)
 #' 
 #' }
 #' 
-addLeafletSidebar <- function(map, options, pane, pane2) {
+addLeafletSidebar <- function(map, options) {
   map$dependencies <- c(map$dependencies, leafletSidebarDependency())
-  invokeMethod(map, getMapData(map), 'addLeafletSidebar', options, pane, pane2)
+  invokeMethod(map, getMapData(map), 'addLeafletSidebar', options)
+}
+
+
+#' Add panel to leaflet sidebar
+#'
+#' @inheritParams addLeafletSidebar
+#' @param tab_name character the name of the tab
+#' @param position character: top or bottom
+#'
+#' @return modified map
+#' @export
+addPanel <- function(map, layerId, tab_name, pane, position = "top") {
+  map$dependencies <- c(map$dependencies, leafletSidebarDependency())
+  invokeMethod(map, getMapData(map), 'addPanel', layerId, tab_name, position)
 }
 
 # sidebar.addPanel(panelContent);
--- inst/htmlwidgets/lib/leaflet-sidebar/leaflet-sidebar-binding.js
+++ inst/htmlwidgets/lib/leaflet-sidebar/leaflet-sidebar-binding.js
@@ -1,21 +1,17 @@
-LeafletWidget.methods.addLeafletSidebar = function(options, pane, pane2) {
+LeafletWidget.methods.addLeafletSidebar = function(options) {
   (function() {
-    var sidebar = L.control.sidebar('sidebar', options).addTo(this); // id and options
-    /* add a new panel */
-    var panelContent = {
-      id: 'userinfo',                     // UID, used to access the panel
-      tab: 'Tab1',  // content can be passed as HTML string,
-      pane: pane,        // DOM elements can be passed, too
-      position: 'top'                  // vertical alignment, 'top' or 'bottom'
-    };
-    var panelContent2 = {
-      id: 'userinfo2',                     
-      tab: 'Tab2',  
-      pane: pane2,    
-      position: 'top'               
-    }; 
-    
-  sidebar.addPanel(panelContent); sidebar.addPanel(panelContent2); sidebar.open('userinfo');
+    var sidebar = L.control.sidebar("sidebar", options).addTo(this); sidebar.open(); // id and options
   }).call(this);
 };
 
+LeafletWidget.methods.addPanel = function(layer_id, tab_name, pane, position) {
+  (function() {
+    var panelContent = {
+      id: layer_id,   
+      tab: tab_name,  
+      pane: pane,     
+      position: position    
+    };
+  sidebar.addPanel(panelContent); sidebar.open();
+  }).call(this);
+};
